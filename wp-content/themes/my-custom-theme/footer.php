ac10e417b75e65e2b4296c9852809c8298eb9cdf
Thanh Pham <thanhpt6@fpt.com>
2025-11-03 14:25:13 +0800

    [TP] Update footer

--- wp-content/themes/my-custom-theme/footer.php
+++ wp-content/themes/my-custom-theme/footer.php
@@ -6,11 +6,11 @@
 
 <footer>
     <div class="footer-content">
-        <p>&copy; <?php echo date("Y"); ?> My Custom Blog. All rights reserved.</p>
+        <p>&copy; <?php echo date("Y"); ?> Tuno's Blog. All rights reserved.</p>
         <ul class="social-media">
-            <li><a href="https://facebook.com" target="_blank">Facebook</a></li>
-            <li><a href="https://twitter.com" target="_blank">Twitter</a></li>
-            <li><a href="https://instagram.com" target="_blank">Instagram</a></li>
+            <li><a href="https://www.facebook.com/tuyetngoc.dinh" target="_blank" rel="noopener">Facebook</a></li>
+            <li><a href="https://www.linkedin.com/in/tuyet-ngoc-dinh-b87494144/" target="_blank" rel="noopener">LinkedIn</a></li>
+            <li><a href="https://www.instagram.com/tunotuyetngoc/" target="_blank" rel="noopener">Instagram</a></li>
         </ul>
     </div>
 </footer>
