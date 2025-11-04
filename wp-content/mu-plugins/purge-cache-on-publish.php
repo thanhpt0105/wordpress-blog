6aa173ade16aa57fe5dcb3ba867d7731e63a2287
Thanh Pham <thanhpt6@fpt.com>
2025-11-05 01:25:07 +0800

    [TP] Update cache issue

--- wp-content/mu-plugins/purge-cache-on-publish.php
+++ wp-content/mu-plugins/purge-cache-on-publish.php
@@ -8,6 +8,19 @@ if (!defined('ABSPATH')) {
 	return;
 }
 
+function acme_prime_cache_urls(array $urls) {
+	foreach ($urls as $url) {
+		if (!$url) {
+			continue;
+		}
+
+		wp_remote_get(add_query_arg('_cache_prime', time(), $url), array(
+			'timeout'  => 3,
+			'blocking' => false,
+		));
+	}
+}
+
 add_action('transition_post_status', function ($new_status, $old_status, $post) {
 	if ($new_status !== 'publish' || $old_status === 'publish' || wp_is_post_revision($post)) {
 		return;
@@ -24,6 +37,21 @@ add_action('transition_post_status', function ($new_status, $old_status, $post) 
 	}
 
 	if (function_exists('wp_remote_get')) {
-		wp_remote_get(add_query_arg('cachebust', time(), home_url('/')));
+		$urls = array(
+			home_url('/'),
+			get_permalink($post),
+		);
+
+		$terms = get_the_terms($post, 'category');
+		if (!is_wp_error($terms) && $terms) {
+			foreach ($terms as $term) {
+				$term_link = get_term_link($term);
+				if (!is_wp_error($term_link)) {
+					$urls[] = $term_link;
+				}
+			}
+		}
+
+		acme_prime_cache_urls(array_unique(array_filter($urls)));
 	}
 }, 10, 3);
